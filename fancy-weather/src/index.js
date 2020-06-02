0c06430879e8a43a58974b18a9224e4441ba8187
Alexey_Yudin <aviordream@gmail.com>
2020-06-02 22:36:31 +0300

    docs: add info for crosscheckers

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,6 +1,8 @@
 import { userEventHandler, initStartState } from './js/control';
+import infoClickHandler from './js/utils/info';
 
 window.onload = async () => {
   initStartState();
   userEventHandler();
+  infoClickHandler();
 };
